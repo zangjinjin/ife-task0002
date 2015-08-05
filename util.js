9889a4841f40bd1cfdffe48f0271ccea138356de
zangjinjin <zangjinjin0220@163.com>
2015-08-05 18:25:04 +0800

    Create util.js

---
+++ util.js
@@ -0,0 +1,31 @@
+function isArray(arr) {
+  if(arr.constructor===Array){ return ture;}
+  else {return false;}
+}
+function isFunction(fn) {
+  if(typeof(fn)=="function"){ return ture;}
+  else {return false;}
+}
+function cloneObject(src) {
+  if(src==''||typeof src!='object')
+    return src;
+  }
+  if(src instanceof Date){
+    var clone=new Date(src.getDate());
+    return clone;
+  }
+  if(src instanceof Array){
+    var clone=[];
+    for(var i=0,len=src.length;i<len;i++){
+      clone[i]=src[i];
+    }
+     return src;
+    if(src instanceof Object){
+      var clone={};
+      for(var key in src){
+        clone[key]=cloneObject(src[key]);
+      }
+      return clone;
+    }
+ 
+}
