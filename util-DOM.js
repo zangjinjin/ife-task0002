a86babdd98d37c4ca3f94111542256a0ed3ca656
zangjinjin <zangjinjin0220@163.com>
2015-08-06 18:29:14 +0800

    Create util-DOM.js

---
+++ util-DOM.js
@@ -0,0 +1,8 @@
+// 为element增加一个样式名为newClassName的新样式
+function addClass(element, newClassName) {
+    element.className+=" "+newClassName;
+}
+
+function removeClass(element, oldClassName) {
+    // your implement
+}
