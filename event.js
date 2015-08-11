ac22e38886fd966ea2dd76357124e7d77418af19
zangjinjin <zangjinjin0220@163.com>
2015-08-11 18:21:01 +0800

    Update event.js

--- event.js
+++ event.js
@@ -41,3 +41,15 @@ var eventUtil={
 		}
 	},
 }
+//通过classname获得DOM元素
+function getByClass(clsName,parent){
+	var oParent=parent?document.getElementById(parent):document,
+		   eles=[],
+		   elements=oParent.getElementsByTagName('*');
+	for(var i=0,l=elements.length;i<l;i++){
+		if(elements[i].calssName==calsName){
+			eles.push(elements[i]);
+		}
+	}
+	return eles;
+}
